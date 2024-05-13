baba3fc2a9db898e08d9053eb8b70de1eb68d567
Asmaa-204 <asmaa204211261@gmail.com>
2024-05-14 01:00:34 +0300

    💄 (UI): Add some styles, Edit API endpoints

--- Frontend/Pages/SearchPage.jsx
+++ Frontend/Pages/SearchPage.jsx
@@ -7,12 +7,12 @@ import styles from "./SearchPage.module.css";
 import logo from "../assets/duck.png";
 import duckDive from "../assets/duckDive.png";
 
-function SearchPage() {
+function SearchPage({searchTerm, setSearchTerm}) {
   return (
     <div className={styles.container}>
       <img src={logo} alt="logo" height={240} />
       <img src={duckDive} height={60} />
-      <Search />
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
     </div>
   );
 }
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import SearchPage from "../Pages/SearchPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ResultsPage from "../Pages/ResultsPage";
+import { useState } from "react";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SearchPage />} />
-        <Route path="/results" element={<ResultsPage />} />
+        <Route path="/" element={<SearchPage searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>} />
+        <Route path="/results" element={<ResultsPage searchTerm={searchTerm}/>} />
       </Routes>
     </BrowserRouter>
   );
