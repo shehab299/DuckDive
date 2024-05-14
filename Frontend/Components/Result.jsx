328a98e3d15bb751248ba6a30341f29ce190cce9
Asmaa-204 <asmaa204211261@gmail.com>
2024-05-14 15:35:51 +0300

    💄 (UI): Highlight searchTerm in snippet

--- Frontend/Components/Result.jsx
+++ Frontend/Components/Result.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import styles from "./Result.module.css"; // Adjust the import as needed
+import styles from "./Result.module.css";
 
 function Result({ result }) {
-  // Regular expression to find single quoted words
   const regex = /'(\w+)'/g;
 
-  // Replace single quoted words with a bold version
   const formattedDescription = result.description.replace(
     regex,
     (match, p1) => `<strong>${p1}</strong>`
